3b0022ed74716ea9f33ab6b984d61199b69624c1
Waidhoferj <jwaidhof@calpoly.edu>
2021-08-31 13:53:51 -0700

    moved global head elements to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,18 @@
 <script>
-    import Nav from "$lib/Nav.svelte"
-    import Footer from "$lib/Footer.svelte"
+	import Nav from '$lib/Nav.svelte';
+	import Footer from '$lib/Footer.svelte';
 </script>
 
-<Nav/>
-<slot></slot>
-<Footer/>
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=swap"
+		rel="stylesheet"
+	/>
+	<meta name="theme-color" content="#2992e5" />
+</svelte:head>
+
+<Nav />
+<slot />
+<Footer />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,14 +18,7 @@
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=swap"
-		rel="stylesheet"
-	/>
 	<title>Cal Poly CSAI</title>
-	<meta name="theme-color" content="#2992e5" />
 	<meta
 		name="description"
 		content="CSAI is a Cal Poly SLO club that uses artificial intelligence to solve impactful engineering problems."
