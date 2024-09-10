4c5c7a2a4fc21b67f4c8d4eba6b47b1842283419
sharon liang <sharonliang617@gmail.com>
2024-09-09 22:30:40 -0700

    updated pfps for hannah, ameer, sharon

---
+++ src/lib/merch/product/ProductDisplayGrid.svelte
@@ -0,0 +1,22 @@
+<!-- product display grid TODO: implement a hover effect when product is selected -->
+<script>
+    import ProductCard from "./ProductCard.svelte";
+    export let products;
+</script>
+
+<div class="grid-container">
+    {#each products as product}
+      <ProductCard {product} />
+    {/each}
+</div>
+
+<style>
+    .grid-container{
+        margin-top: 60px;
+        padding: 96px;
+        display: grid;
+        grid-template-columns: repeat(3, 0.33fr);
+        font-family: 'Inter', sans-serif;
+        gap: 50px;
+    }
+</style>
