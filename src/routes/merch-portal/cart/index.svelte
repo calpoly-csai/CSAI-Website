012570682d1e252ac8f280c847fed865a09c8f5a
shaaronl <sharonliang617@gmail.com>
2024-09-16 23:14:28 -0700

    feat: created checkout page

---
+++ src/routes/merch-portal/cart/index.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Checkout from "$lib/merch/cart/Checkout.svelte";
+</script>
+
+<div class="top">
+    <Checkout/>
+</div>
+
+<style>
+    @import '../../../scss/global.scss';
+	@import '../../../scss/utils.scss';
+
+    .top{
+        margin-top: 100px;
+    }
+</style>
