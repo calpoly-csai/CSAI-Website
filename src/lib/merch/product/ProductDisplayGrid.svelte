24a2d01bfe87dcf2fc94bc02b044183c9bd6be2b
shaaronl <sharonliang617@gmail.com>
2024-10-11 00:21:55 -0700

    feat: product display grid, product card, product individual page

--- src/lib/merch/product/ProductDisplayGrid.svelte
+++ src/lib/merch/product/ProductDisplayGrid.svelte
@@ -1,3 +1,4 @@
+<!-- product display grid -->
 <script>
     import ProductCard from "./ProductCard.svelte";
     export let products;
@@ -18,4 +19,27 @@
         font-family: 'Inter', sans-serif;
         gap: 50px;
     }
+    /* tablets */
+    @media (max-width: 1024px) {
+        .grid-container {
+            padding: 20px 60px 96px 60px;
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+
+    @media (max-width: 1254px) {
+        .grid-container {
+            padding: 0px 60px 96px 60px;
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+
+    /* phones */
+    @media (max-width: 768px) {
+        .grid-container {
+            padding: 0px 50px 96px 20px;
+            grid-template-columns: 1fr;
+        }
+    }
+
 </style>
