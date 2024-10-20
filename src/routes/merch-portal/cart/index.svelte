0f68788398e6cf3fba4327f13a08065bd13a4850
shaaronl <sharonliang617@gmail.com>
2024-10-20 03:01:51 -0700

    feat: implement back navigation button

--- src/routes/merch-portal/cart/index.svelte
+++ src/routes/merch-portal/cart/index.svelte
@@ -1,8 +1,16 @@
 <script>
     import Checkout from "$lib/merch/cart/Checkout.svelte";
+    import { ArrowLeftIcon } from 'svelte-feather-icons';
+
+    const handleBack = () => {
+		window.history.back();
+	};
 </script>
 
 <div class="top">
+    <button class="back-button" on:click={handleBack}>
+        <ArrowLeftIcon />
+    </button>
     <Checkout/>
 </div>
 
@@ -11,6 +19,28 @@
 	@import '../../../scss/utils.scss';
 
     .top{
-        margin-top: 100px;
+        margin-top: 90px;
     }
+
+    .back-button {
+		background: transparent;
+		border: none;
+		color: black;
+		cursor: pointer;
+        margin-left: 50px;
+	}
+
+	.back-button:hover {
+		background-color: rgba(107, 110, 167, 0.1);
+		border-radius: 50%;
+	}
+
+    @media (max-width: 1020px) {
+		.back-button {
+            left: 0;
+            margin-left: 0;
+            margin-bottom: 30px;
+	    }
+    }
+
 </style>
